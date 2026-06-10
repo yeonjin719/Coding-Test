03396cac62e2698e701eddad2fd1a0ecb806d66f
Yeonjin Kim <kyj0719@gmail.com>
2026-06-10 14:42:47 +0900

    feat: 리트코드 허브로 전환

--- leetcode/containsDuplicate.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        test = set(nums)
-        if (len(test) == len(nums)):
-            return False
-        else:
-            return True
--- leetcode/palindromeNumber.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        new = str(x)[::-1]
-
-        if (str(x) == new):
-            return True
-        else:
-            return False
