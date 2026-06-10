e62f1e6260953d9fe410cb88d006ccefe2202940
Yeonjin Kim <kyj0719@gmail.com>
2026-06-10 14:40:18 +0900

    Time: 7 ms (63.59%), Space: 19.4 MB (18.27%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        new = str(x)[::-1]
+
+        if (str(x) == new):
+            return True
+        else:
+            return False
